6c2108e994c2c93a98e4bf27b624f6f3003718bd
namnvhustk61 <79648470+namnvhustk61@users.noreply.github.com>
2023-02-13 22:59:39 +0700

    2/13/2023: Init splash screen 2

--- app/src/main/java/com/example/dowloadprinter/SplashScreen.java
+++ app/src/main/java/com/example/dowloadprinter/SplashScreen.java
@@ -2,7 +2,10 @@ package com.example.dowloadprinter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        CountDownTimer countDownTimer = new CountDownTimer(5000, 300) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(SplashScreen.this, " Dowload Printerest",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+            }
+        };
+        countDownTimer.start();
     }
 }
